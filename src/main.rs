3993fc449c9cdf545298fe3e9b3807aa7b0aa8b4
Pedro <pedro.miguel.lopes951@gmail.com>
2022-06-26 17:42:59 +0100

    Finish testing chapter

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     let name = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello {}!", &name)
-// } Chapter 3.3.2
-
-pub async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().await.unwrap()
-}
+//! main.rs
+use zero2prod::run;
+use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    // use crate::health_check;
-    #[tokio::test]
-    async fn health_check_succeeds() {
-        let response = health_check().await;
-        // This requires changing the return type of `health_check`
-        // from `impl Responder` to `HttpResponse` to compile
-        // You also need to import it with `use actix_web::HttpResponse`!
-        assert!(response.status().is_success())
-    }
+run(TcpListener::bind("127.0.0.1:0").unwrap())?.await
 }
